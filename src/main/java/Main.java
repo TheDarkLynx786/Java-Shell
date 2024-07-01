cd62b4c814535f8892895fcfc0c8eaa426678883
Murtaza Haque <MurtazaHaque@my.unt.edu>
2024-07-01 13:10:35 -0500

    REPL stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        out.print("$ ");
         
         Scanner scanner = new Scanner(System.in);
         
         while (true) { 
             String input = scanner.nextLine();
             out.println(input + ": command not found");
+            out.print("$ ");
         }
         
     }
