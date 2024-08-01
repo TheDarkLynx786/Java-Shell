f3caae321876f3585aef73e0c42f8f3cae6300b1
Murtaza Haque <142961672+TheDarkLynx786@users.noreply.github.com>
2024-08-01 10:04:45 -0500

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import static java.lang.System.getenv;
 import static java.lang.System.out;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
@@ -12,22 +15,42 @@ public class Main {
         
         while (true) { 
             String input = scanner.nextLine();
-            if(input.equals(cmds[0] + " 0")) break;
-            else if (input.substring(0,4).equals(cmds[1])) {
-                out.print(input.substring(5,input.length()) + "\n");
-            } else if (input.substring(0,4).equals(cmds[2])){
-                String toCmpr = input.substring(5,input.length());
+            //Checker Code
+            if(input.equals(cmds[0] + " 0")) break;   //Exit
+            else if (input.substring(0,4).equals(cmds[1])) {   //Echo Builtin
+                out.print(input.substring(5, input.length()) + "\n");
+            } else if (input.substring(0,4).equals(cmds[2])){   //Type Builtin
+                
+                String toCmpr = input.substring(5, input.length());
                 boolean valid = false;
-                for (String cmd : cmds) {
+                for (String cmd : cmds) {   //Iterate through cmds list; check for builtin
                     if(cmd.equals(toCmpr)) valid = true;
                 }
+                
+                //Check for valid path
+                String foundPath = Pathfinder(toCmpr);
+                
                 if(valid) out.println(toCmpr + " is a shell builtin");
+                else if (!"".equals(foundPath)) {
+                    out.println(toCmpr + " is " + foundPath);
+                }
                 else out.println(toCmpr + ": not found");
+
+
             } else {
                 out.println(input + ": command not found");
             }
+
             out.print("$ ");
         }
         
+    }
+    static private String Pathfinder(String file) {
+        for (String path : getenv("PATH").split(":")) {
+            Path filePath = Path.of(path, file);
+            if (Files.isRegularFile(filePath)) return filePath.toString();
+        }
+
+        return ""; 
     }
 }
